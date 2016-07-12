f7464e2c55c7ac68f23da98ad885f23feb9a3d43
Dane Jordan <dane.e.jordan@gmail.com>
2016-07-12 15:48:03 -0700

    Full stack running

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,6 +6,6 @@ module.exports = function(app, express) {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../../client'));
+  app.use(express.static(__dirname + '/../../../poller'));
   app.use('/node_modules', express.static(__dirname + '/../../node_modules'));
 };
